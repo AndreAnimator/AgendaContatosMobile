aa6a131d692e0c673ea37993a85d7ee5841f9283
AndreAnimator <98904347+AndreAnimator@users.noreply.github.com>
2023-11-09 10:52:48 -0300

    AAAAAAA

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBzcuhSZD6nD_lJ8yMUFcppL-QxEV2IaUQ",
+
+    authDomain: "agendacontatosmobileta.firebaseapp.com",
+  
+    projectId: "agendacontatosmobileta",
+  
+    storageBucket: "agendacontatosmobileta.appspot.com",
+  
+    messagingSenderId: "82139206391",
+  
+    appId: "1:82139206391:web:0d2431cca0c94a3d3cb9f2"
+  }
+};
